03539fac36d8e9e1a1cea668201ceb6cce1918ad
João Victor <santosjoao.dev@gmail.com>
2021-07-12 19:45:01 -0300

    adicionando component header

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header";
+import { Container } from "../../components/Header/style";
+
+export default function Home(): JSX.Element {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
