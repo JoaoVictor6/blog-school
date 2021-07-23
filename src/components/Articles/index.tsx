541e2e9694076a7601e4ba2a67e603dc59dd13b7
João Victor <santosjoao.dev@gmail.com>
2021-07-23 20:39:24 -0300

    iniciando o componente article

---
+++ src/components/Articles/index.tsx
@@ -0,0 +1,11 @@
+import { ArticleTitle, Main, Container } from "./style";
+
+export default function Articles(): JSX.Element {
+  return (
+    <Main>
+      <Container id="articles">
+        <ArticleTitle>Artigos</ArticleTitle>
+      </Container>
+    </Main>
+  );
+}
