f1722c370fab3c6187d67e37531d213b8387361a
João Victor <santosjoao.dev@gmail.com>
2021-07-29 14:33:35 -0300

    aprimorando o componente button

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,16 +1,24 @@
 import styled from "styled-components";
 
-export const ButtonElement = styled.button`
+interface ButtonProps {
+  transparent: boolean;
+}
+
+export const ButtonElement = styled.a`
+  text-decoration: none;
+  display: block;
   border-style: none;
   width: 370px;
   border: 3px solid #fff;
   border-radius: 50px;
-  background: rgba(1, 5, 6, 0.34);
+  background: ${({ transparent }: ButtonProps) =>
+    transparent ? "rgba(1, 5, 6, 0.34)" : "#2E2E2E"};
   cursor: pointer;
   transition: all 0.2s ease-in-out;
 
   &:hover {
-    background: rgba(1, 5, 6, 0.5);
+    background: ${({ transparent }: ButtonProps) =>
+      transparent ? "rgba(1, 5, 6, 0.5)" : "#2E2E2E"};
   }
   &:active {
     transform: scale(1.01);
@@ -22,7 +30,7 @@ export const Container = styled.div`
   display: flex;
   justify-content: space-between;
 
-  > p {
+  & > p {
     font-family: "Poppins", sans-serif;
     font-weight: 500;
     font-size: 24px;
